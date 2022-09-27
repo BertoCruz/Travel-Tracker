0a904fe716f0eb0095289c0a997145cb51af22c8
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-09-27 11:31:06 -0600

    micro: Add error handling for login page

--- src/scripts.js
+++ src/scripts.js
@@ -13,7 +13,7 @@ function instantiateAllData() {
             travelersData = data[0].travelers;
             tripsData = data[1].trips;
             destinationsData = data[2].destinations;
-            // console.log("TRAVELERS DATA====",travelersData);
+            console.log("TRAVELERS DATA====",travelersData);
             // console.log("TRIPS DATA=====", tripsData);
             // console.log("DESTINATIONS DATA======", destinationsData);
             // currentTraveler = new Traveler(
@@ -31,8 +31,11 @@ function instantiateAllData() {
 }
 
 //query selectors
+const login = document.querySelector("#login");
 const loginForm = document.querySelector("#loginForm");
+const loginMessage = document.querySelector("#loginMessage");
 const welcomeMessage = document.querySelector("#welcomeMessage");
+const mainTrips = document.querySelector("#mainTrips");
 const upcomingTrips = document.querySelector("#upcomingTrips");
 
 //event handlers
@@ -40,15 +43,60 @@ window.addEventListener("load", instantiateAllData);
 loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const loginData = new FormData(e.target);
-    console.log(loginData.get("username-value"));
-    const travelID = loginData.get("username-value");
-    console.log(trips.checkIfUserExists(travelID.slice(8)));
+    // console.log(loginData.get("username-value"));
+    // const travelID = loginData.get("username-value");
+
+    showHideLoginForm();
+    // if (
+    //     verifyLogin(loginData.get("username-value"), loginData.get("password-value")) !== `invalid`
+    // ) {
+    //     instantiateAllData();
+    //     showHideLoginForm();
+    // } else {
+    //     e.target.reset();
+    // }
+    
 });
 
 
 //render to DOM
+function verifyLogin(username, password) {
+    let idValidation;
+    if (!username.includes("traveler")){
+        loginMessage.innerHTML = "* username not found, please try again.";
+        return `invalid`;
+    } else if (password !== "travel"){
+        loginMessage.innerHTML = "* incorrect password, please try again.";
+        return `invalid`;
+    } else if (username.includes("traveler") && password === "travel") {
+        loginMessage.innerHTML = "";
+        idValidation = allTravelers.checkIfTravelerExists(parseInt(username.slice(8)));
+    }
+
+    if (idValidation === `Traveler ID ${parseInt(username.slice(8))} exists.`) {
+        loginID = parseInt(username.slice(8));
+        loggedIn = true;
+    } else if (idValidation === `Traveler ID ${parseInt(username.slice(8))} does not exist.`) {
+        loginMessage.innerHTML = `* ${username} not found, please try again.`;
+        return `invalid`;
+    }
+
+}
+
+function showHideLoginForm() {
+    if (welcomeMessage.classList.value === "hidden") {
+        login.classList.add("hidden");
+        welcomeMessage.classList.remove("hidden");
+        mainTrips.classList.remove("hidden");
+    } else if (login.classList.value === "hidden") {
+        welcomeMessage.classList.add("hidden");
+        mainTrips.classList.add("hidden");
+        login.classList.remove("hidden");
+    }
+};
+
 function loadTraveler() {
-    renderWelcomeMessage();
+    //renderWelcomeMessage();
     // renderUpcomingTrips();
 }
 
